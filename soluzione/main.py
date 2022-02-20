d8f4230cf12df4fc1b303fdf7bda21637f515421
Gabriele Costanzo <g.costanzo.web@gmail.com>
2022-02-21 00:50:20 +0100

    Conclusione soluzione guidata, con commenti

--- soluzione/main.py
+++ soluzione/main.py
@@ -1,20 +1,30 @@
 # Setup: popola il dizionario "rubrica" con dei valori finti generati online
-
 from ottieni_rubrica import ottieni_rubrica
 rubrica = ottieni_rubrica()
-prossimo_id = len(rubrica)
+
+prossimo_id = len(rubrica)  # indica il prossimo id libero utilizzabile per aggiungere un contatto
 
 #############################################################################
-# Definisci qui i tuoi comandi
+# Definizione dei comandi
+# N.B. ogni comando possiede un "docstring", una stringa compresa tra """tripli apici""",
+# resa disponibile accedendo alla proprietà .__doc__ della funzione.
+
+# Esempio: "aggiungi.__doc__" ci ritorna la documentazione della funzione "aggiungi"
+
+#####
+# Funzione AGGIUNGI, per creare un nuovo contatto
 def aggiungi():
     """Aggiungi contatto"""
 
-    global rubrica, prossimo_id
+    global rubrica, prossimo_id # la keyword "global" indica che useremo variabili dichiarate fuori dalla funzione
+    
+    # Per ogni campo del contatto, richiediamo l'informazione usando "input"
     nome = input("Nome contatto: ")
     cognome = input("Cognome contatto: ")
     email = input("Email: ")
     num_casa = input("Telefono di casa: ")
     num_cell = input("Cellulare: ")
+    # Inseriamo i campi raccolti in un nuovo dizionario, con la struttura dei contatti
     nuovo_contatto = {
         "nome": nome,
         "cognome": cognome,
@@ -25,33 +35,47 @@ def aggiungi():
         }
     }
 
+    # Inseriamo il nuovo contatto nel prossimo posto libero della rubrica
     rubrica[str(prossimo_id)] = nuovo_contatto
+    # Aggiorniamo il prossimo posto libero
     prossimo_id += 1
     print(f"Ho aggiunto {nome} {cognome} alla tua rubrica!")
 
-
+#####
+# Funzione MODIFICA, per modificare un contatto esistente
 def modifica():
     """Modifica contatto"""
 
     global rubrica
 
+    # Ipotesi: bisogna già sapere l'id dell'utente da modificare
     id = input("Quale utente vuoi cercare? (Inserisci id): ")
+    # Controlliamo se l'id è presente tra le chiavi della rubrica
     if id in rubrica:
-
+        #... e in tal caso, salviamo in una variabile il contatto corrispondente a quell'id
         contatto = rubrica[id]
 
+        # Ripetiamo lo stesso codice della funzione "AGGIUNGI", ma con una modifica:
+
         nome = input(f"Nome contatto ({contatto['nome']}): ")
-        if nome=="":
+        # Se anziché inserire un valore premiamo INVIO, ottenendo una stringa vuota,
+        # intendiamo questa azione come voler "mantenere il campo per com'era già",
+        # e in tal caso poniamo la stringa uguale a com'era nella variabile "contatto"
+        if nome=="":    
             nome = contatto['nome']
+
         cognome = input(f"Cognome contatto ({contatto['cognome']}): ")
         if cognome=="":
             cognome = contatto['cognome']
+
         email = input(f"Email ({contatto['email']}): ")
         if email=="":
             email = contatto['email']
+
         num_casa = input(f"Telefono di casa ({contatto['telefono']['casa']}): ")
         if num_casa=="":
             num_casa = contatto['telefono']['casa']
+
         num_cell = input(f"Cellulare ({contatto['telefono']['cellulare']}): ")
         if num_cell=="":
             num_cell = contatto['telefono']['cellulare']
@@ -66,65 +90,123 @@ def modifica():
             }
         }
 
+        # In questo caso assegniamo il "nuovo_contatto" nella posizione dell'id scelto
         rubrica[id] = nuovo_contatto
         print(f"Ho modificato {nome} {cognome} nella tua rubrica!")
     else:
+        # Se l'id non è presente nelle chiavi della rubrica...
         print("Id non valido!")
 
 
+#####
+# Funzione RICERCA, per conoscere l'id di un contatto dato nome e/o cognome
 def ricerca():
     """Cerca contatto"""
 
     global rubrica
 
+    # La stringa può contenere il nome, il cognome o nome e cognome separati da uno spazio
     stringa = input("Chi stai cercando? ").lower()
-    lista = []
+    # N.B. perché ".lower()"? Perché conviene sempre porre tutte le stringhe da paragonare per somiglianza
+    # in un unico "case": o tutto "uppercase" (maiuscolo) o tutto "lowercase" (minuscolo)
+
+    lista = [] # Teniamo una lista vuota dove metteremo i "match" con la stringa inserita
+
+    # rubrica.items ritorna una LISTA di TUPLE di 2 elementi, nella forma [(chiave1, valore1), (chiave2, valore2), ...]
     for id, contatto in rubrica.items():
-        if (stringa in contatto["nome"].lower()) or (stringa in contatto["cognome"].lower()) or (stringa in (contatto["nome"] +" "+ contatto["cognome"]).lower()):
+        # ad ogni iterazione si ha: id=chiave (da "0" a "99"), contatto=valore {nome, cognome, ...}
+
+        # Grazie ad Alberto Bella per il consiglio di "any"!
+        # La funzione "any" è un "OR ad N-vie", ovvero ritorna True se una qualunque delle sue N condizioni è True
+        if any([
+            stringa in contatto["nome"].lower(),    # La stringa è nel nome 
+            stringa in contatto["cognome"].lower(), # La stringa è nel cognome
+            stringa in (contatto["nome"] +" "+ contatto["cognome"]).lower() # La stringa è in "nome cognome"
+        ]):
+            # Se uno qualunque dei controlli passa, inseriamo la coppia (chiave, valore) nella lista dei match
             lista.append((id, contatto))
 
+    # Se la lista contiene dei match...
     if len(lista)>0:
+        # Nuovamente, per ogni coppia (chiave, valore) nella lista dei match, con id=chiave e contatto=valore
         for id, contatto in lista:
+            # Stampiamo l'id, che ci servirà per modificare o eliminare il contatto, assieme ad alcune sue info
             print(id, ":", contatto["nome"], contatto["cognome"])
     else:
+        # Altrimenti, non ho trovato contatti!
         print("Non ho trovato nessun risultato!")
 
 
+#####
+# Funzione ELIMINA, per eliminare un contatto dalla rubrica
+def elimina():
+    """Elimina contatto"""
 
+    global rubrica
+
+    # Ipotesi: bisogna già sapere l'id dell'utente da eliminare
+    id = input("Quale utente vuoi eliminare? (Inserisci id): ")
+    # Controlliamo se l'id è presente tra le chiavi della rubrica
+    if id in rubrica:
+        #... e in tal caso, salviamo in una variabile il contatto corrispondente a quell'id
+        contatto = rubrica[id]
+
+        print(f'Elimino {contatto["nome"]} {contatto["cognome"]}...')
+
+        del rubrica[id]
+
+    else:
+        # Se l'id non è presente nelle chiavi della rubrica...
+        print("Id non valido!")
+
+
+#####
+# Funzione TERMINA, chiude il programma
 def termina():
     """Termina programma"""
 
-    global continua
+    global continua # Da questa variabile dipende il loop principale del programma
     print("Grazie per avere usato la mia rubrica!")
-    continua = False
+    continua = False    # Settando "continua" a False interrompiamo il loop del programma
 
 #############################################################################
 
+# Lista di tutti i comandi utilizzabili del programma
 comandi = [
     # Aggiungi qui i tuoi comandi
     ricerca,
     aggiungi,
     modifica,
+    elimina,
     termina
 ]
 
-continua = True
+continua = True # Fintanto che è True, il programma continuerà a chiederci nuove operazioni sulla rubrica
 
 while continua:
     print("-"*20)
     print("GESTIONE RUBRICA - Comandi disponibili:\n")
+
+    # La funzione "enumerate" trasforma ogni elemento "comando" in una coppia (indice, comando), dove
+    # "indice" rappresenta l'indice numerico di quel comando nell'array (da 0 a len(comandi)-1)
     for indice, comando in enumerate(comandi):
-        print(f"[{indice}]", "-", comando.__doc__)
+        print(f"[{indice}]", "-", comando.__doc__) # Per ogni comando stampiamo la guida, nella forma: [0] - Nome comando
     print("\n"+"-"*20)
-    i = input("Quale comando vuoi usare? ")
+
+
+    i = input("Quale comando vuoi usare? ") # Chiediamo il numero corrispondente al comando da eseguire
     try:
-        i = int(i)
-        if i<0 or i>=len(comandi):
-            print("Indice fuori range!\n\n")
-            continue
-        else:
-            print("\n\n")
-            comandi[i]()
-    except Exception as e:
-        print(e)
-        continue
+        i = int(i)  # Proviamo a convertire la stringa "i" nel numero intero corrispondente
+    except:
+        # Se otteniamo un errore, vuol dire che l'utente non ha immesso una stringa convertibile ad intero
+        print("Non è stato inserito un numero!\n\n")
+        continue # Ricominciamo il loop
+
+    if i<0 or i>=len(comandi): # Possiamo accettare solo numeri da 0 a len(comandi)-1
+        print("Indice fuori range!\n\n")
+        continue    # Ricominciamo il loop
+    
+    else:
+        # Se arriviamo qui, eseguiamo il comando di indice i
+        print("\n\n")
+        comandi[i]() # equivalente di "nome_comando_i_esimo()"
--- soluzione/ottieni_rubrica.py
+++ soluzione/ottieni_rubrica.py
@@ -1,14 +1,19 @@
 import requests #py -3 -m pip install requests
 
 def ottieni_rubrica():
-    url = "https://randomuser.me/api/?results=100"
-    req = requests.get(url)
-    if req.status_code != 200:
-        return {}
+    url = "https://randomuser.me/api/?results=100"  # questa API usa un parametro ?results=N per generare N contatti random in un JSON
+    req = requests.get(url) # effettuiamo la chiamata all'API
+    if req.status_code != 200:  # status 200 indica una richiesta HTTP con risposta con successo
+        return {}   # se non è 200, ritorniamo un dizionario vuoto
     else:
+        # se è 200, richiamando il metodo .json sull'oggetto richiesta, otteniamo il dizionario corrispondente
         diz = req.json()
+        # Modifichiamo il dizionario per ottenere una rubrica:
+        # - con "enumerate(diz"results"), otteniamo una lista del tipo [(0, contatto0), (1, contatto1), ...]
+        # - assegniamo i due valori di ogni coppia a due variabili di iterazione "chiave, valore"
+        # - modelliamo ogni campo di "valore" per creare una nuova struttura, più semplice
         rubrica_diz = {
-            str(chiave) : 
+            str(chiave) : # Usiamo come chiave l'indice numerico del contatto enumerato, trasformato in stringa
             {
                 "nome": valore["name"]["first"], 
                 "cognome": valore["name"]["last"],
@@ -18,4 +23,5 @@ def ottieni_rubrica():
                     "cellulare": valore["cell"]
                 }
         } for chiave, valore in enumerate(diz["results"])}
+        # Ritorniamo la rubrica con la nostra struttura
         return rubrica_diz
